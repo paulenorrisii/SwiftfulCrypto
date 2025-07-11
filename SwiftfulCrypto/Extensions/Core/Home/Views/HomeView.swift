b64dff95b09398665a2f49d86b973c74f0e848b3
Paul Norris <paul.norrisii@gmail.com>
2025-07-10 17:45:37 -0700

    Lesson #2, 19:48 OK

--- SwiftfulCrypto/Extensions/Core/Home/Views/HomeView.swift
+++ SwiftfulCrypto/Extensions/Core/Home/Views/HomeView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
     var body: some View {
         ZStack {
             // background layer
@@ -17,18 +20,34 @@ struct HomeView: View {
             // content layer
             VStack {
                 HStack {
-                    CircleButtonView(iconName: "info")
+                    CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                        .animation(.none)
+                        .background(
+                            CircleButtonAnimationView(animate: $showPortfolio)
+                        )
                     Spacer()
-                    Text("Header")
+                    Text(showPortfolio ? "Portfolio" : "Live Prices")
+                        .font(.headline)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.theme.accent)
+                        .animation(.none)
                     Spacer()
                     CircleButtonView(iconName: "chevron.right")
+                        .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                        .onTapGesture {
+                            withAnimation(.spring()){
+                                showPortfolio.toggle()
+                            }
+                        }
                 }
-                Spacer(minLength: 0)
+                        .padding(.horizontal)
+                    Spacer(minLength: 0)
+                }
+                
             }
-            
         }
     }
-}
+
 
 #Preview {
     NavigationView {
