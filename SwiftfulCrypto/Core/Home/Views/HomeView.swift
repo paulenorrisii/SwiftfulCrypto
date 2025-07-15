16263e55349055e34f2500f2e33de9293ab37924
Paul Norris <paul.norrisii@gmail.com>
2025-07-14 18:42:36 -0700

    Lesson #5 completed OK

--- SwiftfulCrypto/Core/Home/Views/HomeView.swift
+++ SwiftfulCrypto/Core/Home/Views/HomeView.swift
@@ -23,15 +23,15 @@ struct HomeView: View {
             VStack {
                 homeHeader
                 
+                columnTitles
+                
                 if !showPortfolio {
-                    List {
-                        ForEach(vm.allCoins) { coin in
-                            CoinRowView(coin: DeveloperPreview.instance.coin, showHoldingsColumn: false)
-                            
-                        }
-                    }
+                    allCoinsList
                     .transition(.move(edge: .leading))
-                    .listStyle(PlainListStyle())
+                }
+                if showPortfolio {
+                    portfolioCoinsList
+                        .transition(.move(edge: .trailing))
                 }
                     Spacer(minLength: 0)
                 }
@@ -77,4 +77,39 @@ extension HomeView {
         }
                 .padding(.horizontal)
     }
+    
+    private var allCoinsList: some View {
+        List {
+            ForEach(vm.allCoins) { coin in
+                CoinRowView(coin: DeveloperPreview.instance.coin, showHoldingsColumn: false)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+            }
+        }
+        .listStyle(PlainListStyle())
+    }
+    
+    private var portfolioCoinsList: some View {
+        List {
+            ForEach(vm.portfolioCoins) { coin in
+                CoinRowView(coin: DeveloperPreview.instance.coin, showHoldingsColumn: true)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+            }
+        }
+        .listStyle(PlainListStyle())
+    }
+    
+    private var columnTitles: some View {
+        HStack {
+            Text("Coin")
+            Spacer()
+            if showPortfolio {
+                Text("Holdings")
+            }
+            Text("Price")
+            .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)                }
+        .font(.caption)
+        .foregroundColor(Color.theme.secondaryText)
+        .padding(.horizontal)
+    }
 }
+
