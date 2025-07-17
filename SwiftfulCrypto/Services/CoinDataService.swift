22f6a4afe8d8cd5630faae0024ce823debfcc26f
Paul Norris <paul.norrisii@gmail.com>
2025-07-16 19:23:23 -0700

    Completed lesson #6, still getting repeated entries for Bitcoin

--- SwiftfulCrypto/Services/CoinDataService.swift
+++ SwiftfulCrypto/Services/CoinDataService.swift
@@ -4,6 +4,13 @@
 //
 //  Created by Paul Norris on 7/15/25.
 //
+/*
+ Comment on web page.
+They have changed the API, you need to make an account and get a demo API Key. Then change your URL to include "?x_cg_demo_api_key=YOUR_API_KEY" at the end. I also had to remove "priceChangePercentage24HInCurrency" from my model to get it to work.
+ 
+ Not sure this is accurate. Nick's app still runs.
+ 
+ */
 
 import Foundation
 import Combine
@@ -19,32 +26,18 @@ class CoinDataService {
     }
     
     private func getCoins() {
-        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else { return }
+        guard let url = URL(string:
+                                "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h"
+                            //                             "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h?x_cg_demo_api_key=CG-SzMBAP7QRHVamGb2CgF1kcVE"
+                            
+        ) else { return }
         
-        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
-            .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { (output) -> Data in
-                
-                guard let response = output.response as? HTTPURLResponse,
-                      response.statusCode >= 200 && response.statusCode < 300
-                else {
-                    throw URLError(.badServerResponse)
-                }
-                return output.data
-            }
-            .receive(on: DispatchQueue.main)
+        coinSubscription = NetworkingManager.download(url: url)
             .decode(type: [CoinModel].self, decoder: JSONDecoder())
-            .sink( receiveCompletion: { (completion) in
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            }, receiveValue: { [weak self] (returnedCoins) in
-                self?.allCoins = returnedCoins
+            
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnCoins) in
+                self?.allCoins = returnCoins
                 self?.coinSubscription?.cancel()
-                
             })
     }
 }
---
+++ SwiftfulCrypto/Utilities/NetworkingManager.swift
@@ -0,0 +1,57 @@
+//
+//  NetworkingManager.swift
+//  SwiftfulCrypto
+//
+//  Created by Paul Norris on 7/16/25.
+//
+
+import Foundation
+import Combine
+
+class NetworkingManager {
+    
+    enum NetworkingError: LocalizedError {
+        case badURLResponse (url: URL)
+        case unknown
+        
+        var errorDescription: String? {
+            switch self {
+            case .badURLResponse(url: let url ):
+                return "[🔥]Bad URL Response. for URL: \(url)"
+            case .unknown:
+                return "[⁉️]Unknown Error"
+            }
+        }
+    }
+    
+    static func download(url: URL) -> AnyPublisher<Data, Error> {
+//        Data, Publishers.ReceiveOn<Publishers.TryMap<Publishers.SubscribeOn<URLSession.DataTaskPublisher, DispatchQueue>, Data>, DispatchQueue> {
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap( { try handleURLResponse(output: $0, url: url) } )
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+    
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+  // debugging
+        // throw NetworkingError.badURLResponse(url: url)
+        guard let response = output.response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode < 300
+        else {
+            throw NetworkingError.badURLResponse(url: url)
+        }
+        return output.data
+    }
+    
+    
+    static func handleCompletion(completion: Subscribers.Completion<Error>) {
+        switch completion {
+        case .finished:
+            break
+        case .failure(let error):
+            print("error is ", error.localizedDescription)
+        }
+    }
+}
+
