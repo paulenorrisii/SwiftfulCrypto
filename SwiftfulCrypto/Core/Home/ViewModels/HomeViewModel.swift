b77e2ea91fdecd7993e20868c00d3e033992decd
Paul Norris <paul.norrisii@gmail.com>
2025-07-23 19:25:22 -0700

    Lesson #11 completed successfully

--- SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -27,11 +27,51 @@ class HomeViewModel:ObservableObject {
     }
     
     func addSubscribers() {
-        dataService.$allCoins
+//        dataService.$allCoins
+//            .sink { [weak self] (returnedCoins) in
+//                self?.allCoins = returnedCoins
+//            }
+//            .store(in: &cancellables)
+        
+        // updates allCoins
+        
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: 0.5, scheduler: DispatchQueue.main)
+            .map(filterCoins)
+//            .map { (text, startingCoins) -> [CoinModel] in
+//                
+//                guard !text.isEmpty else {
+//                    return startingCoins
+//                }
+//                
+//                let lowercasedText = text.lowercased()
+//                
+//                return startingCoins.filter { (coin) -> Bool in
+//                    return coin.name.lowercased().contains(lowercasedText) ||
+//                    coin.symbol.lowercased().contains(lowercasedText) ||
+//                    coin.id.lowercased().contains(lowercasedText)
+//                }
+//            }
             .sink { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
+                
             }
             .store(in: &cancellables)
+        }
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+            
+            guard !text.isEmpty else {
+                return coins
+            }
+            
+            let lowercasedText = text.lowercased()
+            
+            return coins.filter { (coin) -> Bool in
+                return coin.name.lowercased().contains(lowercasedText) ||
+                coin.symbol.lowercased().contains(lowercasedText) ||
+                coin.id.lowercased().contains(lowercasedText)
+            }
         }
     }
 
