cf7c5cd7155e0d2bdd079bccbb983629237f73b5
Paul Norris <paul.norrisii@gmail.com>
2025-07-27 19:21:09 -0700

    Lesson #13, 17:25 in

---
+++ SwiftfulCrypto/Services/MarketDataService.swift
@@ -0,0 +1,33 @@
+//
+//  MarketDataService.swift
+//  SwiftfulCrypto
+//
+//  Created by Paul Norris on 7/27/25.
+//
+
+import Foundation
+
+import Combine
+
+class MarketDataService {
+
+    @Published var marketData: MarketDataModel? = nil
+    
+    var marketDataSubscription: AnyCancellable?
+    
+    init() {
+        getData()
+    }
+    
+    private func getData() {
+        guard let url = URL(string: "URL: https://api.coingecko.com/api/v3/global") else { return }
+        
+        marketDataSubscription = NetworkingManager.download(url: url)
+            .decode(type: GlobalData.self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedGlobalData) in
+                self?.marketData = returnedGlobalData.data
+                self?.marketDataSubscription?.cancel()
+            })
+    }
+}
