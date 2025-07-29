efc792094cd7a5fbcc34e136c1d4ae591bc89819
Paul Norris <paul.norrisii@gmail.com>
2025-07-28 19:09:21 -0700

    Lesson #14, 5:58 in, not working

---
+++ SwiftfulCrypto/Core/Home/Views/PortfolioView.swift
@@ -0,0 +1,25 @@
+//
+//  PortfolioView.swift
+//  SwiftfulCrypto
+//
+//  Created by Paul Norris on 7/28/25.
+//
+
+import SwiftUI
+
+struct PortfolioView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                VStack(alignment: .leading) {
+                       Text("Hi")
+                }
+            }
+            .navigationTitle("Edit Portfolio")
+        }
+    }
+}
+
+#Preview {
+    PortfolioView()
+}
