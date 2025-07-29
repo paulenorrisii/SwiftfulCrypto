e1fa702ce96ef1cab250e9cec8298e7b77f48849
Paul Norris <paul.norrisii@gmail.com>
2025-07-28 17:52:26 -0700

    Lesson #13, 24:31 in, OK!

--- SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -11,74 +11,102 @@ import Combine
 class HomeViewModel:ObservableObject {
     
     @Published var statistics: [StatisticModel] = [
-            StatisticModel(title: "Title", value: "Value",percentageChange: 1),
-            StatisticModel(title: "Title", value: "Value"),
-            StatisticModel(title: "Title", value: "Value"),
-            StatisticModel(title: "Title", value: "Value",percentageChange: -1),
-        ]
+//        StatisticModel(title: "Title", value: "Value",percentageChange: 1),
+//        StatisticModel(title: "Title", value: "Value"),
+//        StatisticModel(title: "Title", value: "Value"),
+//        StatisticModel(title: "Title", value: "Value",percentageChange: -1),
+    ]
     
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     
     @Published var searchText: String = ""
     
-    private let dataService = CoinDataService()
+    private let coinDataService = CoinDataService()
+    private let marketDataService = MarketDataService()
     private var cancellables = Set<AnyCancellable>()
     
     init() {
         addSubscribers()
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-//            self.allCoins.append(DeveloperPreview.instance.coin)
-//            self.portfolioCoins.append(DeveloperPreview.instance.coin)
-//        }
+        //        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+        //            self.allCoins.append(DeveloperPreview.instance.coin)
+        //            self.portfolioCoins.append(DeveloperPreview.instance.coin)
+        //        }
     }
     
     func addSubscribers() {
-//        dataService.$allCoins
-//            .sink { [weak self] (returnedCoins) in
-//                self?.allCoins = returnedCoins
-//            }
-//            .store(in: &cancellables)
+        //        dataService.$allCoins
+        //            .sink { [weak self] (returnedCoins) in
+        //                self?.allCoins = returnedCoins
+        //            }
+        //            .store(in: &cancellables)
         
         // updates allCoins
         
         $searchText
-            .combineLatest(dataService.$allCoins)
+            .combineLatest(coinDataService.$allCoins)
             .debounce(for: 0.5, scheduler: DispatchQueue.main)
             .map(filterCoins)
-//            .map { (text, startingCoins) -> [CoinModel] in
-//                
-//                guard !text.isEmpty else {
-//                    return startingCoins
-//                }
-//                
-//                let lowercasedText = text.lowercased()
-//                
-//                return startingCoins.filter { (coin) -> Bool in
-//                    return coin.name.lowercased().contains(lowercasedText) ||
-//                    coin.symbol.lowercased().contains(lowercasedText) ||
-//                    coin.id.lowercased().contains(lowercasedText)
-//                }
-//            }
+        //            .map { (text, startingCoins) -> [CoinModel] in
+        //
+        //                guard !text.isEmpty else {
+        //                    return startingCoins
+        //                }
+        //
+        //                let lowercasedText = text.lowercased()
+        //
+        //                return startingCoins.filter { (coin) -> Bool in
+        //                    return coin.name.lowercased().contains(lowercasedText) ||
+        //                    coin.symbol.lowercased().contains(lowercasedText) ||
+        //                    coin.id.lowercased().contains(lowercasedText)
+        //                }
+        //            }
             .sink { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
                 
             }
             .store(in: &cancellables)
-        }
+        
+        marketDataService.$marketData
+            .map { (marketDataModel) -> [StatisticModel] in
+                var stats: [StatisticModel] = []
+                
+                guard let data = marketDataModel else {
+                    return stats
+                }
+                
+                let marketCap = StatisticModel(title: "Market Cap", value: data.marketCap, percentageChange: data.marketCapChangePercentage24HUsd)
+                let volume = StatisticModel(title: "24h Volume", value: data.volume)
+                let btcDominance = StatisticModel(title: "BTC Dominance", value: data.btcDominance)
+                let portfolio = StatisticModel(title: "Portfolio Value", value: "$0.00", percentageChange: 0)
+                
+                stats.append(contentsOf: [
+                    marketCap,
+                    volume,
+                    btcDominance,
+                    portfolio
+                ])
+                    return stats
+            }
+            .sink { [weak self] (returnedStats) in
+                self?.statistics = returnedStats
+            }
+            .store(in: &cancellables)
+    }
+    
     private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
-            
-            guard !text.isEmpty else {
-                return coins
-            }
-            
-            let lowercasedText = text.lowercased()
-            
-            return coins.filter { (coin) -> Bool in
-                return coin.name.lowercased().contains(lowercasedText) ||
-                coin.symbol.lowercased().contains(lowercasedText) ||
-                coin.id.lowercased().contains(lowercasedText)
-            }
+        
+        guard !text.isEmpty else {
+            return coins
+        }
+        
+        let lowercasedText = text.lowercased()
+        
+        return coins.filter { (coin) -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+            coin.symbol.lowercased().contains(lowercasedText) ||
+            coin.id.lowercased().contains(lowercasedText)
         }
     }
-
+    
+}
