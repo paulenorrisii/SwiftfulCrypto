ce98dd73fd8583ff9bb0841bd10f6de0f364d525
Paul Norris <paul.norrisii@gmail.com>
2025-07-29 19:09:24 -0700

    Lesson 30:07, mostly OK, xmark does not exit

--- SwiftfulCrypto/Core/Home/Views/PortfolioView.swift
+++ SwiftfulCrypto/Core/Home/Views/PortfolioView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct PortfolioView: View {
     
     @EnvironmentObject private var vm: HomeViewModel
+    @State private var selectedCoin: CoinModel? = nil
+    @State private var quantityText: String = ""
+    
     
     var body: some View {
         NavigationView {
@@ -17,19 +20,11 @@ struct PortfolioView: View {
                 VStack(alignment: .leading, spacing: 0) {
                     SearchBarView(searchText: $vm.searchText)
                     
-                    ScrollView(.horizontal, showsIndicators: false, content: {
-                        LazyHStack(spacing: 10) {
-                            ForEach(vm.allCoins) { coin in
-                                CoinLogoView(coin: coin)
-                                    .frame(width: 75)
-                                    .padding(4)
-                                    .background(
-                                        RoundedRectangle(cornerRadius: 10)
-                                            .stroke(Color.theme.green, lineWidth: 1)
-                                       )
-                            }
-                        }
-                })
+                    coinLogoList
+                    
+                    if selectedCoin != nil {
+                        portfolioInputSection
+                    }
                 }
             }
             
@@ -47,5 +42,66 @@ struct PortfolioView_Previews: PreviewProvider {
     static var previews: some View {
         PortfolioView()
             .environmentObject(dev.homeVM)
+    }
+}
+
+extension PortfolioView {
+    
+    private var coinLogoList: some View {
+        ScrollView(.horizontal, showsIndicators: false, content: {
+            LazyHStack(spacing: 10) {
+                ForEach(vm.allCoins) { coin in
+                    CoinLogoView(coin: coin)
+                        .frame(width: 75)
+                        .padding(4)
+                        .onTapGesture{
+                            withAnimation(.easeIn) {
+                                selectedCoin = coin
+                            }
+                        }
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(selectedCoin?.id == coin.id ?
+                                        Color.theme.green: Color.clear, lineWidth: 1)
+                        )
+                }
+            }
+            .frame(height: 120)
+            .padding(.leading)
+        })
+    }
+    
+    private func getCurrentValue() -> Double {
+        if let quantity = Double(quantityText) {
+            return quantity * (selectedCoin?.currentPrice ?? 0)
+        }
+        return 0
+    }
+    
+    private var portfolioInputSection: some View {
+        VStack(spacing: 20){
+            HStack{
+                Text("Current price of \(selectedCoin?.symbol.uppercased() ?? ""):")
+                Spacer()
+                Text(selectedCoin?.currentPrice.asCurrencyWith6Decimals() ?? "")
+            }
+Divider()
+            HStack{
+                Text("Amount holding:")
+                Spacer()
+                TextField("Ex: 1.4",text: $quantityText)
+                    .multilineTextAlignment(.trailing)
+                    .keyboardType(.decimalPad)
+            }
+             Divider()
+            HStack {
+                Text("Current value:")
+                Spacer()
+                Text(getCurrentValue().asCurrencyWith2Decimals())
+            }
+        }
+        .animation(.none)
+        .padding()
+        .font(.headline)
     }
 }
